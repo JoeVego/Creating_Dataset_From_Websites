834166a9d55aa7b261262412b471ee6dcecf856a
Ayrat <joevegojv@gmail.com>
2023-12-27 17:41:30 +0300

    получил локацию элементов кнопок

--- delete.py
+++ delete.py
@@ -3,18 +3,30 @@ from PIL import Image
 import numpy as np
 
 def lets_try():
-    driver = webdriver.Firefox(executable_path='S:\\geckodriver\\geckodriver.exe')
+    PATH_PC = 'S:\\geckodriver\\geckodriver.exe'
+    PATH_NB = 'C:\\Users\\Admin\\Desktop\\Учебные материалы\\вкр\\geckodriver.exe'
+    driver = webdriver.Firefox()
     driver.get("https://ya.ru/")
 
-    driver.save_screenshot('S:\\test.png')
+    PATH_PIC_PC = 'S:\\test.png'
+    PATH_PIC_NB = 'C:\\Users\\Admin\\Desktop\\Учебные материалы\\вкр\\test.png'
+    # driver.get_full_page_screenshot_as_png()
+    driver.save_screenshot(PATH_PIC_NB)
+
+    # html = driver.page_source
+    # print(html)
+    # driver.quit()
+
+    elements = driver.find_elements("xpath", "//button | //input[@type='submit'] | // input[@type='button']")
+    print("elements = ", elements)
+    for elem in elements:
+        print(elem.location)
+
     driver.quit()
 
-    img =  Image.open('S:\\test.png')
+    img = Image.open(PATH_PIC_NB)
+    img = img.convert('RGB')
     numpy_array = np.array(img)
     print(numpy_array.shape)
-    # убрать 4ую размерность ?
-    # numpy_array2 = numpy_array[:, :, :-1]
-    # print(numpy_array.shape)
 
-    # гит проект
     # - beautiful soap - parsing html
