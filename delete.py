bb83868a23f19363bf74f9e6cf84035be678e95f
Ayrat <joevegojv@gmail.com>
2023-12-28 11:07:48 +0300

    Установка размеров окна и универсального локатора кнопок

--- delete.py
+++ delete.py
@@ -11,22 +11,48 @@ def lets_try():
     PATH_PIC_PC = 'S:\\test.png'
     PATH_PIC_NB = 'C:\\Users\\Admin\\Desktop\\Учебные материалы\\вкр\\test.png'
     # driver.get_full_page_screenshot_as_png()
+    #
+    # Установка размера окна
+    # # Setting the window size to 1200 * 800
+    driver.set_window_size(1212, 1291)
     driver.save_screenshot(PATH_PIC_NB)
 
     # html = driver.page_source
     # print(html)
     # driver.quit()
 
-    elements = driver.find_elements("xpath", "//button | //input[@type='submit'] | // input[@type='button']")
+    # разные поиски по классуЦсс, или find_element_by_tag_name
+    elements = driver.find_elements("xpath", "//button | "
+                                             "//input[@type='submit'] | "
+                                             "//input[@type='button'] | "
+                                             "//input[@class='submit'] | "
+                                             "//input[@class='button'] |  "
+                                             "//a[@class='submit']  |"
+                                             "//a[@type='button']  |"
+                                             "//a[@type='submit']  |"
+                                             "//a[@class='button']  |"
+                                             "//div[@class='button']   |"
+                                             "//div[@type='button']   |"
+                                             "//div[@class='submit']   | "
+                                             "//li[@class='button'] |"
+                                             "//div[@type='submit'] | "
+                                             "//li[@type='submit'] |"
+                                             "//li[@type='button'] |"
+                                             "//li[@class='submit']")
     print("elements = ", elements)
     for elem in elements:
-        print(elem.location)
+        # dict.get(key, default) - возвращает значение ключа, но если его нет,
+        # не бросает исключение, а возвращает default (по умолчанию None).
+        location = elem.location
+        # print(location)
+        print("x =", elem.location['x'], ", y =", elem.location['y'])
+
 
     driver.quit()
 
     img = Image.open(PATH_PIC_NB)
     img = img.convert('RGB')
     numpy_array = np.array(img)
-    print(numpy_array.shape)
+    print("after convert to RGB =", numpy_array.shape)
 
     # - beautiful soap - parsing html
